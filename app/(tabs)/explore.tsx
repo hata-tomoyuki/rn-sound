1eb9d9f0af59e41fd51ba2410c45561d55a6c7e4
hata-tomoyuki <h.tomo0602@gmail.com>
2025-11-01 10:17:13 +0900

    クリック音

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,15 +1,25 @@
+import { useAudioPlayer } from 'expo-audio';
 import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { Button, Platform, StyleSheet } from 'react-native';
 
-import { Collapsible } from '@/components/ui/collapsible';
 import { ExternalLink } from '@/components/external-link';
 import ParallaxScrollView from '@/components/parallax-scroll-view';
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
+import { Collapsible } from '@/components/ui/collapsible';
 import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Fonts } from '@/constants/theme';
 
+const sfxSource = require('@/assets/sounds/sound.mp3');
+
 export default function TabTwoScreen() {
+  const sfx = useAudioPlayer(sfxSource);
+  const playSfx = () => {
+    try {
+      sfx.seekTo(0);
+      sfx.play();
+    } catch {}
+  };
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
@@ -94,6 +104,7 @@ export default function TabTwoScreen() {
           ),
         })}
       </Collapsible>
+      <Button title="Play Sound Effect" onPress={playSfx} />
     </ParallaxScrollView>
   );
 }
